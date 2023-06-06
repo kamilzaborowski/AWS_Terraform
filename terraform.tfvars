5e6f186ac30892315782c122bc8422e82f4c5909
Kamil Zaborowski <kamil.zaborowski@o2.pl>
2023-06-06 18:56:11 +0200

    Delete old project files

--- terraform.tfvars
+++
@@ -1,3 +0,0 @@
-do_token = ${DO_PAT}
-pvt_key = data.local_file.pvt.content
-pub_key = data.local_file.pub.content
--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "do_token" { 
-    sensitive = true
-    }
-variable "pvt_key" {
-    sensitive = true
-    }
-variable "pub_key" {
-    sensitive = false
-}
